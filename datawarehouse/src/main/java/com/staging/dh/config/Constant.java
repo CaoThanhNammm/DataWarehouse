6de9ded7aeb0bda4107d5623f1844ec387102fac
CaoThanhNammm <21130448@st.hcmuaf.edu.vn>
2024-11-16 09:35:17 +0700

    thêm class Constant.java để cấu hình hằng số, thêm tài liệu chạy bằng tay

---
+++ datawarehouse/src/main/java/com/staging/dh/config/Constant.java
@@ -0,0 +1,8 @@
+package com.staging.dh.config;
+
+public class Constant {
+    public static final int WAITING = 1;
+    public static final int RUNNING = 2;
+    public static final int COMPLETE = 3;
+    public static final int FAILED = 4;
+}
--- datawarehouse/src/main/java/com/staging/dh/service/control/LogService.java
+++ datawarehouse/src/main/java/com/staging/dh/service/control/LogService.java
@@ -1,21 +1,24 @@
 package com.staging.dh.service.control;
 
+import com.staging.dh.config.Constant;
 import com.staging.dh.model.control.Log;
 import com.staging.dh.repository.control.LogRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
-public class LogService implements ILogService{
+public class LogService implements ILogService {
     @Autowired
     private LogRepo dao;
 
     @Override
     public Log add(Log log) {
-        if(!dao.existsByWebsiteIdAndStatus(log.getWebsiteId(), log.getStatus())){
+        if (log.getStatus().getId() != Constant.WAITING) {
+            dao.save(log);
+            return log;
+        }
+
+        if (!dao.existsByWebsiteIdAndStatus(log.getWebsiteId(), log.getStatus())) {
             dao.save(log);
             return log;
         }
