98b49207b4bbdebcc3d033c2c0ed4c5063215b18
lengocha <21130340@st.hcmuaf.edu.vn>
2024-12-03 17:26:28 +0700

    update send mail

--- sql/logs.sql
+++ sql/logs.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : bainguoita
+ Source Server         : Localhost
  Source Server Type    : MySQL
  Source Server Version : 100428
  Source Host           : localhost:3306
- Source Schema         : control_db
+ Source Schema         : control
 
  Target Server Type    : MySQL
  Target Server Version : 100428
  File Encoding         : 65001
 
- Date: 25/11/2024 16:28:04
+ Date: 14/11/2024 18:51:34
 */
 
 SET NAMES utf8mb4;
@@ -22,20 +22,28 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `logs`;
 CREATE TABLE `logs`  (
-  `log_id` int NOT NULL AUTO_INCREMENT,
-  `file_id` int NULL DEFAULT NULL,
-  `status` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `log_message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
-  `timeStart` datetime(6) NULL DEFAULT current_timestamp(6),
+  `id` int NOT NULL AUTO_INCREMENT,
+  `message` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `quantity` int NULL DEFAULT NULL,
+  `statusId` int NULL DEFAULT NULL,
+  `websiteId` int NULL DEFAULT NULL,
+  `timeStart` datetime(6) NULL DEFAULT NULL,
   `timeEnd` datetime(6) NULL DEFAULT NULL,
-  PRIMARY KEY (`log_id`) USING BTREE,
-  INDEX `file_id`(`file_id` ASC) USING BTREE,
-  CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `config` (`file_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 60 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `FK3d5jkj33k7m98tttikpva61fh`(`statusId` ASC) USING BTREE,
+  INDEX `FKdc4xf5e5rg9n4avr0sk1b1d4x`(`websiteId` ASC) USING BTREE,
+  CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`statusId`) REFERENCES `status` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `logs_ibfk_2` FOREIGN KEY (`websiteId`) REFERENCES `control` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 53 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of logs
 -- ----------------------------
-INSERT INTO `logs` VALUES (1, 1, 'Success', 'Load dữ liệu về file csv', '2024-11-25 02:03:52.000000', '2024-11-25 02:04:30.000000');
+INSERT INTO `logs` VALUES (29, 'starting scrape data', 0, 2, 4, '2024-11-07 22:41:10.000000', NULL);
+INSERT INTO `logs` VALUES (30, 'complete scrape data', 1206, 3, 4, '2024-11-07 22:41:10.000000', '2024-11-08 00:00:01.000000');
+INSERT INTO `logs` VALUES (31, 'complete scrape data', 958, 3, 1, '2024-11-07 22:41:09.000000', '2024-11-08 00:23:58.000000');
+INSERT INTO `logs` VALUES (32, 'starting scrape data', 0, 2, 1, '2024-11-11 20:05:07.000000', NULL);
+INSERT INTO `logs` VALUES (34, 'complete scrape data', 1135, 3, 4, '2024-11-11 22:27:43.000000', '2024-11-11 23:36:36.000000');
+INSERT INTO `logs` VALUES (35, 'Load Data From Staging to Data Warehouse', 1359, 3, NULL, '2024-11-14 00:02:03.000000', '2024-11-14 00:02:08.000000');
 
 SET FOREIGN_KEY_CHECKS = 1;
